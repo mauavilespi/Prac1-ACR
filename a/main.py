ad5a96a47be0dae43550b5ed7ec93d2bc1bf8b81
mauavilespi <mauavilespi@outlook.com>
2022-02-24 19:48:13 -0600

    Obtenemos nombres de los libros

--- a/main.py
+++ a/main.py
@@ -1,1 +1,10 @@
-print ("First Commit")
+# Librerias
+import os # Para obtener los nombres de los libros
+
+### El proceso padre lee los nombres de los libros ###
+nombresLibros = [] # Se crea un arreglo donde se guardaran los nombres
+directorioActual = os.getcwd() # Obtener el directorio actual
+directorioActual = directorioActual + '/a/Libros' # Directorio donde se guardan los libros
+nombresLibros = os.listdir(directorioActual) # Se guardan los nombres de los libros
+print("Directorio: \n", directorioActual)
+print("Libros: \n", nombresLibros)
